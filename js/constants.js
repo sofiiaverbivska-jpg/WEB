e18cb2f0f939f32b11220ef5c941c45bc2603e66
vr_sofiia <sonaverbivska@gmail.com>
2026-05-06 09:07:10 +0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,23 @@
-// final version
+const trendingGames = [
+  {
+    id: 1,
+    image: "img/game1.svg",
+    followers: 40
+  },
+  {
+    id: 2,
+    image: "img/game2.svg",
+    followers: 40
+  },
+  {
+    id: 3,
+    image: "img/game3.svg",
+    followers: 40
+  },
+  {
+    id: 4,
+    image: "img/game4.svg",
+    followers: 40
+  }
+];
+
